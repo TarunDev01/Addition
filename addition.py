207773696a4ff63c7be630f7d2f0af6bab725f48
Tarun dev <devtarunmagadi@gmail.com>
2025-11-24 16:36:50 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,7 +1,15 @@
-def add(a,b):
+import sys
+
+def add(a, b):
     return a + b
 
-if __name__ == "__main__":
-    x = 10
-    y = 20
-    print("Sum:", add(x, y))
+if name == "main":
+
+    if len(sys.argv) != 3:
+        print("Usage: python add.py <num1> <num2>")
+        sys.exit(1)
+
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+    
+    print("Sum:", add(x, y))
