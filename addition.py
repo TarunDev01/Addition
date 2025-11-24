98ea4bebfead46922df036e37f52394d8716a5c2
Tarun dev <devtarunmagadi@gmail.com>
2025-11-24 16:39:20 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -4,12 +4,12 @@ def add(a, b):
     return a + b
 
 if name == "main":
-
     if len(sys.argv) != 3:
         print("Usage: python add.py <num1> <num2>")
         sys.exit(1)
 
+    
     x = int(sys.argv[1])
     y = int(sys.argv[2])
-    
-    print("Sum:", add(x, y))
+
+    print("Sum:", add(x, y))
