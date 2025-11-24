737ae258613d756b2f7726a3089df014f03b0e1e
Tarun dev <devtarunmagadi@gmail.com>
2025-11-24 16:40:53 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -3,7 +3,7 @@ import sys
 def add(a, b):
     return a + b
 
-if name == "main":
+if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python add.py <num1> <num2>")
         sys.exit(1)
